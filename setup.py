0bfd7e0df22acda14ab576e3fd5e6b60e50b1a21
Kneasle <kneasle@gmail.com>
2021-02-12 18:23:58 +0000

    Add setup.py and add build files to gitignore

---
+++ setup.py
@@ -0,0 +1,38 @@
+""" Setup script for belltower """
+
+import setuptools
+
+# Read the README as the long description
+with open("README.md") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name="belltower",
+    version="v0.1.0",
+
+    author="Ben White-Horne",
+    author_email="kneasle@gmail.com",
+
+    description="A Pythonic interface to Ringing Room, an online change ringing platform.",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/kneasle/belltower",
+
+    license="MIT",
+    platforms="any",
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests",
+        "python-socketio<5",
+        "python-engineio<4",
+        "websocket-client"
+    ],
+)
